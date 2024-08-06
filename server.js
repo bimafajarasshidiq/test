9009e1148104891cbb84ff7790296c66012527f1
bimafajarasshidiq <bimaf.bf.bf@gmail.com>
2024-08-06 15:37:53 +0900

    menambahkan file koneksi

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+
+//Pengaplikasian Parse/JSon
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.listen(3000, () => {
+    console.log('Server Started on port');
+});
+
